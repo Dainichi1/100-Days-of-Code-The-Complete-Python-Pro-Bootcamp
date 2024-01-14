d619c8b3e1d1e5943d13502f3288bf11433e0557
dainichi79 <96421043+dainichi79@users.noreply.github.com>
2024-01-14 19:06:04 +0100

    Primo commit

--- GIORNO 5-Python Loops/1-Loop.py
+++ GIORNO 5-Python Loops/1-Loop.py
@@ -4,6 +4,5 @@ for fruit in fruits:
   print (fruit + " Torta")
   print (fruits)
   print (len(fruits)) 
-for fruit1 in fruits:
-  print (fruits[fruit1])
+
   
---
+++ GIORNO 5-Python Loops/4-For loop and the range() function.py
@@ -0,0 +1,11 @@
+for number in range(1, 10): #non stampa 10
+    print(number)
+    
+for number in range(1, 10, 3):
+    print("\n", number)
+
+total = 0
+for number in range(1 , 101):
+    total += number
+print (total)
+    
