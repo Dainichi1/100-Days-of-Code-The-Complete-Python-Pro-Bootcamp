739e4618f976c2a53059298eef1d9400af1d9764
dainichi79 <96421043+dainichi79@users.noreply.github.com>
2024-01-27 13:00:31 +0100

    Primo commit

--- GIORNO 9-Dictionaries, Nesting and the Secret auction/3-Nesting lists and Dictionaries.py
+++ GIORNO 9-Dictionaries, Nesting and the Secret auction/3-Nesting lists and Dictionaries.py
@@ -15,3 +15,18 @@ travel_log = {
     "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
     "Germany": ["Berlin", "Hamburg", "Stuttgart"],
 }
+
+# Nest dictionary in a list
+travel_log = [
+    {"country": "France", 
+     "cities_visited": ["Paris", "Lille", "Dijon"], 
+     "total_visits": 12
+     },
+    {
+    "country": "Germany", 
+    "cities_visited": ["Berlin", "Hamburg", "Stuttgart"], 
+    "total_visits": 5
+    },
+]
+
+
