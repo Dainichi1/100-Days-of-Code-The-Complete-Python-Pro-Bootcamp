1661d19cabffa0183941711a494f94021be61173
dainichi79 <96421043+dainichi79@users.noreply.github.com>
2024-01-27 12:52:13 +0100

    Primo commit

---
+++ GIORNO 9-Dictionaries, Nesting and the Secret auction/3-Nesting lists and Dictionaries.py
@@ -0,0 +1,17 @@
+# Nesting
+capitals = {
+    "France": "Paris",
+    "Germany": "Berlin",
+}
+
+# Nesting a list in a dictionary
+travel_log = {
+    "France": ["Paris", "Lille", "Dijon"],
+    "Germany": ["Berlin", "Hamburg", "Stuttgart"],
+}
+
+# Nest a dictionary in a dictionary
+travel_log = {
+    "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
+    "Germany": ["Berlin", "Hamburg", "Stuttgart"],
+}
